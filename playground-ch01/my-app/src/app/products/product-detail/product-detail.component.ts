2eec06cd8926615b05cd7162b997095d98f13206
Marcelo Giorgi <marklazz.uy@gmail.com>
2023-12-18 16:03:55 -0300

    Remove exploration code

--- playground-ch01/my-app/src/app/products/product-detail/product-detail.component.ts
+++ playground-ch01/my-app/src/app/products/product-detail/product-detail.component.ts
@@ -1,11 +1,9 @@
-import { Component, Input, Output, EventEmitter, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css'],
-  encapsulation: ViewEncapsulation.None,
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductDetailComponent {
   @Input() name = '';
@@ -13,10 +11,5 @@ export class ProductDetailComponent {
 
   buy() {
     this.bought.emit(this.name);
-  }
-
-  get productName(): string {
-    console.log(`Get ${this.name}`);
-    return this.name;
   }
 }
