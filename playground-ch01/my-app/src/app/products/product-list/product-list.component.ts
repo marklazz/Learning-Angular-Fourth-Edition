2c998ceebd88501edfd6dcc39ce3635a20d61704
Marcelo Giorgi <marklazz.uy@gmail.com>
2023-12-19 17:41:00 -0300

    Use directives to hide content

--- playground-ch01/my-app/src/app/products/product-list/product-list.component.ts
+++ playground-ch01/my-app/src/app/products/product-list/product-list.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { ProductDetailComponent } from '../product-detail/product-detail.component';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
-  selectedProduct = 'Microphone';
+export class ProductListComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    if (this.productDetail) {
+      console.log('HERE:');
+      console.log(this.productDetail.name);
+    }
+  }
+
+  // selectedProduct = 'Microphone';
+  selectedProduct = '';
+  @ViewChild(ProductDetailComponent) productDetail: ProductDetailComponent | undefined;
 
   onBuy(name: string) {
     window.alert(`You just bought ${name}!`);
