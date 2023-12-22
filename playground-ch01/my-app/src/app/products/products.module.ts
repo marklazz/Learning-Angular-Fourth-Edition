f1cef16cf9c9b63178faaa73075bb94cb97ecf4a
Marcelo Giorgi <marklazz.uy@gmail.com>
2023-12-22 19:02:32 -0300

    Add component added dynamically

--- playground-ch01/my-app/src/app/products/products.module.ts
+++ playground-ch01/my-app/src/app/products/products.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { SortPipe } from './sort.pipe';
+import { ProductHostDirective } from './product-host.directive';
 
 @NgModule({
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    SortPipe
+    SortPipe,
+    ProductHostDirective
   ],
   imports: [
     CommonModule
